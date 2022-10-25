b8077ed6359eef9b427b425c75a141c09fdc68c8
LeartMena <leart.mena@outlook.com>
2022-10-25 14:55:52 +0200

    Projekt finished

--- Siemens Schnuppertag/Siemens Schnuppertag/Program.cs
+++ Siemens Schnuppertag/Siemens Schnuppertag/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System.Linq.Expressions;
 using static System.Net.WebRequestMethods;
+using Newtonsoft.Json;
 
 var _client = new HttpClient();
 
 Console.WriteLine("What do you want to translate?");
 var text = Console.ReadLine();
-Console.WriteLine("In witch languege?");
+Console.WriteLine("In which language?");
 var language = Console.ReadLine();
 
-Console.WriteLine("To be translate: " + text + "\nlanguage " + language);
+Console.WriteLine("To be translate: " + text + "\nlanguage " + language + "\n");
 
 var key = System.Environment.GetEnvironmentVariable("keydeeplapi"); 
 var url = "https://api-free.deepl.com/v2/translate";
@@ -28,5 +29,20 @@ var response = await _client.PostAsync(url, valueJson);
 
 var content = response.Content.ReadAsStringAsync();
 
-Console.WriteLine(content.Result);
+var contentSerialized = JsonConvert.DeserializeObject<DeeplResponse>(content.Result);
 
+Console.WriteLine(contentSerialized.translations[0].text);
+
+Console.WriteLine(contentSerialized.translations[0].detected_source_language);
+
+
+public class DeeplResponse
+{
+    public List<DeeplTranslation> translations { get; set; }
+}
+
+public class DeeplTranslation
+{
+    public string detected_source_language { get; set; }
+    public string text { get; set; }
+}
