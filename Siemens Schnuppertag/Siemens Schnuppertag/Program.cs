20328a0e33ebfa3b68e6b3638e3d041ff4b8e69e
LeartMena <leart.mena@outlook.com>
2022-10-25 12:16:05 +0200

    Man kann eingeben, was für einen Text man übersetzen und  in welche Sprache man ihn übersetzen will

---
+++ Siemens Schnuppertag/Siemens Schnuppertag/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("What do you want to translate?");
+var text = Console.ReadLine();
+Console.WriteLine("In witch languege?");
+var language = Console.ReadLine();
+
+Console.WriteLine("To be translate: " + text + "\nlanguage + language");
